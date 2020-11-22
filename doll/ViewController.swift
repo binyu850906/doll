dcbc26177a2664d9c71101d561b595e131d082c2
binyuxie <binyu850906@gmail.com>
2020-11-22 21:05:02 +0800

    end

--- doll/ViewController.swift
+++ doll/ViewController.swift
@@ -9,11 +9,61 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var bodyImage: UIImageView!
+    
+    @IBOutlet weak var faceImage: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func body1(_ sender: UIButton) {
+        let image = sender.currentBackgroundImage
+        bodyImage.image = image
+    
+    }
+    
+    @IBAction func body2(_ sender: UIButton) {
+        let image = sender.currentBackgroundImage
+        bodyImage.image = image
+    }
+    
+    @IBAction func boy3(_ sender: UIButton) {
+        let image = sender.currentBackgroundImage
+        bodyImage.image = image
 
+    }
+    @IBAction func body4(_ sender: UIButton) {
+        let image = sender.currentBackgroundImage
+        bodyImage.image = image
+
+    }
+    
+    @IBAction func face0(_ sender: UIButton) {
+        let image = sender.currentBackgroundImage
+        faceImage.image = image
+    }
+    
+    @IBAction func face1(_ sender: UIButton) {
+        let image = sender.currentBackgroundImage
+        faceImage.image = image
+    }
+    
+    
+    @IBAction func face2(_ sender: UIButton) {
+        let image = sender.currentBackgroundImage
+        faceImage.image = image
+    }
+    
+    @IBAction func face3(_ sender: UIButton) {
+        let image = sender.currentBackgroundImage
+        faceImage.image = image
+    }
+    
+    
+    
+    
+    
+    
+    
 }
-
